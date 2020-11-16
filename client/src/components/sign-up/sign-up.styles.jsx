217203a9e9899ac91e60aef1d6d8a2eb73f4fc11
ymizrahi <yitzhak.tech@gmail.com>
2020-11-17 01:02:38 +0200

    updating the sign in and sign up styles to be compatible with smaller screens

--- client/src/components/sign-up/sign-up.styles.jsx
+++ client/src/components/sign-up/sign-up.styles.jsx
@@ -4,6 +4,10 @@ export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 380px;
+
+  @media screen and (max-width: 400px) {
+    padding: 0 30px;
+  }
 `;
 
 export const SignUpTitle = styled.h2`
